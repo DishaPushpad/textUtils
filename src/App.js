a75a0647cf2f134be989cb1ebe99e1710a1fdfe5
DishaPushpad <disha.espsofttech@gmail.com>
2023-02-02 19:34:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Alert from "./components/Alert";
+
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import {
+//   BrowserRouter,
+// Routes,
+//   Route
+  
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+
+
+  const showAlert=(message,type)=>{
+setAlert({
+  msg:message,
+  type:type
+})
+setTimeout(() => {
+  setAlert(null)
+}, 3000);
+  }
+
+
+
+  //whether dark mode is enable or not
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#343a40";
+      showAlert("dark mode enabled","success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode enabled","success")
+    }
+  };
+
+//   const [nmode, setnmode] = useState("light");
+// const blueMode=()=>{
+// if(nmode==="light"){
+// setnmode("blue")
+// document.body.style.backgroundColor="#9cb3fb"
+// // setInterval(()=>{
+// //   document.title='textUtils'
+// // },2000);
+// // setInterval(()=>{
+// //   document.title='install'
+// // },1500)
+
+// }else{setnmode("light")
+// document.body.style.backgroundColor="white"}
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+    {/* <BrowserRouter> */}
+       <Navbar title="welcome" mode={mode} toggleMode={toggleMode} /> 
+
+  <Alert alert={alert} />
+      <div className="container my-3">
+      {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About/>}/> */}
+          
+          {/* <Route exact path="/" */}
+          {/* element={  */}
+          <TextForm showAlert={showAlert} heading="enter text"  mode={mode}/>
+          {/* <About/> */}
+          {/* }>  */}
+          {/* </Route> */}
+        {/* </Routes> */}
+
+        {/* <div className="container my-4" />  */}
+      {/* <About mode={mode}  toggleMode={toggleMode} />  */}
+      {/* </BrowserRouter> */}
+      </div>
+    </>
   );
 }
 
